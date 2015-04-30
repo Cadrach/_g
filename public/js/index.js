c91b6b468df7d7fa3d13112415448919b3bc972e
Rachid <rachid@actived.fr>
2015-04-30 16:57:14 +0200

    Isometric camera

--- public/js/index.js
+++ public/js/index.js
@@ -4,26 +4,76 @@ define(["require", "exports", "lib/three"], function (require, exports, three) {
         function Test() {
         }
         Test.prototype.draw = function () {
-            var scene, camera, renderer;
-            var geometry, material, mesh;
+            var info, mesh, renderer, scene, camera, controls;
             init();
-            animate();
+            render();
             function init() {
-                scene = new THREE.Scene();
-                camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
-                camera.position.z = 1000;
-                geometry = new THREE.BoxGeometry(200, 200, 200);
-                material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
-                mesh = new THREE.Mesh(geometry, material);
-                scene.add(mesh);
+                // info
+                info = document.createElement('div');
+                info.style.position = 'absolute';
+                info.style.top = '30px';
+                info.style.width = '100%';
+                info.style.textAlign = 'center';
+                info.style.color = '#fff';
+                info.style.fontWeight = 'bold';
+                info.style.backgroundColor = 'transparent';
+                info.style.zIndex = '1';
+                info.style.fontFamily = 'Monospace';
+                info.innerHTML = 'three.js - Isometric Projection<br/>drag mouse to rotate camera';
+                document.body.appendChild(info);
+                // renderer
                 renderer = new THREE.WebGLRenderer();
                 renderer.setSize(window.innerWidth, window.innerHeight);
                 document.body.appendChild(renderer.domElement);
+                // scene
+                scene = new THREE.Scene();
+                // camera
+                var aspect = window.innerWidth / window.innerHeight;
+                var d = 20;
+                camera = new THREE.OrthographicCamera(-d * aspect, d * aspect, d, -d, 1, 1000);
+                // /////////////////////////////////////////////////////////////////////////
+                // method 1 - use lookAt
+                //camera.position.set( 20, 20, 20 );
+                //camera.lookAt( scene.position );
+                // method 2 - set the x-component of rotation
+                camera.position.set(20, 20, 20);
+                camera.rotation.order = 'YXZ';
+                camera.rotation.y = -Math.PI / 4;
+                camera.rotation.x = Math.atan(-1 / Math.sqrt(2));
+                // /////////////////////////////////////////////////////////////////////////
+                // controls
+                //            controls = new THREE.OrbitControls( camera, renderer.domElement );
+                //            controls.addEventListener( 'change', render );
+                //            controls.noZoom = true;
+                //            controls.noPan = true;
+                //            controls.maxPolarAngle = Math.PI / 2;
+                //            //controls.zoomSpeed = 0.1;
+                // ambient
+                scene.add(new THREE.AmbientLight(0x444444));
+                // light
+                var light = new THREE.PointLight(0xffffff, 0.8);
+                light.position.set(0, 50, 50);
+                scene.add(light);
+                // axes
+                scene.add(new THREE.AxisHelper(40));
+                // grid
+                var geometry = new THREE.PlaneGeometry(100, 100, 10, 10);
+                var material = new THREE.MeshBasicMaterial({ wireframe: true, opacity: 0.5, transparent: true });
+                var grid = new THREE.Mesh(geometry, material);
+                grid.rotation.order = 'YXZ';
+                grid.rotation.y = -Math.PI / 2;
+                grid.rotation.x = -Math.PI / 2;
+                scene.add(grid);
+                // geometry
+                var geometry = new THREE.BoxGeometry(10, 10, 10);
+                // material
+                var material = new THREE.MeshNormalMaterial();
+                // mesh
+                mesh = new THREE.Mesh(geometry, material);
+                mesh.translateX(50);
+                scene.add(mesh);
             }
-            function animate() {
-                requestAnimationFrame(animate);
-                mesh.rotation.x += 0.01;
-                mesh.rotation.y += 0.02;
+            function render() {
                 renderer.render(scene, camera);
             }
         };
